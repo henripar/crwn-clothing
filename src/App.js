86dbf85a01e8486dc192ff0a96b81e4a03a3f03d
Henri Parviainen <henri.parviainen@icloud.com>
2020-05-08 21:24:24 +0300

    updated routing to navigate menu-items with withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/mens' component={MensPage} />
+      </Switch>
     </div>
   );
 }
