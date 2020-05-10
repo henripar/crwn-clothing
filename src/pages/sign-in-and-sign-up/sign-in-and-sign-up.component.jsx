0928faac6615cb3aed954f6b1d1b3ee66c4f2c84
Henri Parviainen <henri.parviainen@icloud.com>
2020-05-10 22:53:16 +0300

    added styles to signin component and installed firebase

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/sign-in.component./sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    SIGN IN
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
